9b6a75a6ca16a9ae470b2a0e8f7834d6be50205b
kb1995 <kristiyan.r.bogdanov@gmail.com>
2020-05-23 17:54:47 +0100

    Import images and articles

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,3 +1,9 @@
+import Swup from "swup";
+import SwupPreloadPlugin from "@swup/preload-plugin";
 import "../styles/index.scss";
+
+const swup = new Swup({
+  plugins: [new SwupPreloadPlugin()],
+});
 
 console.info("Bridgetown is loaded!");
